67c299be5a64fa597a7d50619d5f4b046bb300a1
Brett Poarch <brett.poarch@gmail.com>
2020-03-06 15:10:45 -0800

    complete

--- router.js
+++ router.js
@@ -1,4 +1,6 @@
 import Stories from './pages/stories.js';
+import Item from './pages/item.js';
+import Favorites from './pages/favorites.js';
 
 const router = new Navigo(null, true, '#');
 
@@ -13,7 +15,10 @@ export default class RouterHandler {
 			{ path: '/', page: Stories },
 			{ path: '/new', page: Stories },
 			{ path: '/ask', page: Stories },
-			{ path: '/show', page: Stories }
+			{ path: '/show', page: Stories },
+			{ path: '/item', page: Item },
+			{ path: '/jobs', page: Stories },
+			{ path: '/favorites', page: Favorites }
 		];
 
 		routes.forEach(({ path, page }) => {
